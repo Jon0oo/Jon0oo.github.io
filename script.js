a30d0ff8eba353d6ca5fc6613a2f8ada37ae3c33
Joshi <jonobamxd11@gmail.com>
2024-08-01 19:49:17 +0200

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', function() {
     const scrolled = window.scrollY;
-    document.body.style.backgroundPosition = 'center ' + (scrolled * 0.1) + 'px';
+    document.body.style.backgroundPosition = 'center ' + (scrolled * 0.001) + 'px';
 });
