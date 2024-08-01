afacf962ac1edc668db938bf7e9f439e5a52c039
Joshi <jonobamxd11@gmail.com>
2024-08-01 17:44:38 +0200

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     const image = document.getElementById('movingImage');
-    image.style.transform = `translate(-50%, -${scrollPosition * 0.5}px)`;
+    image.style.transform = `translate(-50%, calc(-50% - ${scrollPosition * 0.5}px))`;
 });
