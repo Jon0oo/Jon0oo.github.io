0c109d5f1ca6277d2b0cc9dc25e8b10d02469960
Joshi <jonobamxd11@gmail.com>
2024-08-01 18:02:41 +0200

    test

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    const image = document.getElementById('movingImage');
-    image.style.transform = `translate(50%, calc(50% - ${scrollPosition}px))`;
-});
+// The function
+var background_image_parallax = function($object, multiplier){
+    multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+      multiplier = 1 - multiplier;
+    var $doc = $(document);
+    $object.css({"background-attatchment" : "fixed"});
+      $(window).scroll(function(){
+        var from_top = $doc.scrollTop(),
+            bg_css = '0px ' +(multiplier * from_top) + 'px';
+        $object.css({"background-position" : bg_css });
+    });
+  };
+  
+  //Just pass the jQuery object
+  background_image_parallax($(".box2"));
+  
+  //optional second value for speed
+  background_image_parallax($(".box3"), 0.25);
