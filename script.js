d8024aa1f26db0c7010a34f59e877f469ec14989
Joshi <jonobamxd11@gmail.com>
2024-08-01 17:41:27 +0200

    test

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 window.addEventListener('scroll', function() {
-    const content = document.querySelector('.content');
-    if (window.scrollY > 100) { // Adjust the scroll value as needed
-        content.classList.add('scrolled');
-    } else {
-        content.classList.remove('scrolled');
-    }
+    const scrollPosition = window.scrollY;
+    const image = document.getElementById('movingImage');
+    image.style.transform = `translate(-50%, -${scrollPosition * 0.5}px)`;
 });
