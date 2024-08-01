7577f34e42229b2bf352e9ed0e3f2f405ce584f8
Joshi <jonobamxd11@gmail.com>
2024-08-01 21:10:47 +0200

    dropdown Menu

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ window.addEventListener('scroll', function() {
     box.style.transform = `translateY(${scrollPosition * 0.2}px)`;
   });
   
+  let lastScrollTop = 0;
+window.addEventListener('scroll', function() {
+  const dropdowns = document.querySelectorAll('.dropdown');
+  let scrollTop = window.scrollY;
+
+  if (scrollTop > lastScrollTop) {
+    dropdowns.forEach(dropdown => {
+      dropdown.style.display = 'none';
+    });
+  }
+  lastScrollTop = scrollTop;
+});
