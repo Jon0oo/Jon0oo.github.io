c5067104f89de72ee0b431ad1d91f6293c65f20e
Joshi <jonobamxd11@gmail.com>
2024-08-01 20:35:03 +0200

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function() {
     const box = document.querySelector('.box');
     const scrollPosition = window.scrollY;
-    box.style.transform = `translate(-50%, calc(-50% + ${scrollPosition * 0.1}px))`;
+    box.style.transform = `translate(-50%, calc(-50% + ${scrollPosition * 1.5}px))`;
 });
