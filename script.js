54e7bf96e0110209e9ae9fa5d732f0a6339674f4
Joshi <jonobamxd11@gmail.com>
2024-08-01 21:41:19 +0200

    undated value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
     const box = document.querySelector('.box');
     const scrollPosition = window.scrollY;
-    box.style.transform = `translateY(${scrollPosition * 0.1}px)`;
+    box.style.transform = `translateY(${scrollPosition * 0.3}px)`;
   });
 
   let lastScrollTop = 0;
