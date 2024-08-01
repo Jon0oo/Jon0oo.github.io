07f57b27eb94a8b21695c0e68d16c1a2bcaf3fcb
Joshi <jonobamxd11@gmail.com>
2024-08-01 21:39:22 +0200

    adjusted values

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.addEventListener('scroll', function() {
     const box = document.querySelector('.box');
     const scrollPosition = window.scrollY;
-    box.style.transform = `translateY(${scrollPosition * 0.2}px)`;
+    box.style.transform = `translateY(${scrollPosition * 0.1}px)`;
   });
-  
+
   let lastScrollTop = 0;
 window.addEventListener('scroll', function() {
   const dropdowns = document.querySelectorAll('.dropdown');
